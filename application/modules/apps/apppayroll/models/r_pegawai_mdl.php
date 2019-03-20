392d3d89142b84e1f6922af80f9d437f1fd8c654
Putra <cristminix@gmail.com>
2019-03-20 10:35:45 +0700

    Fix edit gapok

--- application/modules/apps/apppayroll/models/r_pegawai_mdl.php
+++ application/modules/apps/apppayroll/models/r_pegawai_mdl.php
@@ -8,19 +8,19 @@ require_once 'apppayroll_frontmdl' . EXT;
 class R_Pegawai_Mdl extends Apppayroll_Frontmdl {
     public $tbl                  = 'r_pegawai';
     
-    public function get_min_tgl_terima_year(){ 
-        $rw = $this->db->select('g.mk_peringkat')->from($tbl . ' p')
-                 ->join('r_peg_golongan g','left')
-                 ->order_by('g.kode_golongan','desc')
-                 ->order_by('g.mk_peringkat','desc')
-                 ->get()
-                 ->row();
-        if(!empty($rw)){
-            return $rw->mk_peringkat;
-        }
-        return 0;
-    }
-    public function _get_min_tgl_terima_year(){
+    // public function get_min_tgl_terima_year(){ 
+    //     $rw = $this->db->select('g.mk_peringkat')->from($tbl . ' p')
+    //              ->join('r_peg_golongan g','left')
+    //              ->order_by('g.kode_golongan','desc')
+    //              ->order_by('g.mk_peringkat','desc')
+    //              ->get()
+    //              ->row();
+    //     if(!empty($rw)){
+    //         return $rw->mk_peringkat;
+    //     }
+    //     return 0;
+    // }
+    public function get_min_tgl_terima_year(){
         $this->db->select('min(year(tgl_terima)) maxyear', false);
         $rs = $this->db->get($this->tbl);
         $result = date('Y');
